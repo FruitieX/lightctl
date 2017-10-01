aecbd3f160de017aa16073cd30b742d1c7970e02
Rasmus Eskola <fruitiex@gmail.com>
2017-10-01 19:45:13 +0300

    Colorloop scene

---
+++ plugins/scenes/colorloop.js
@@ -0,0 +1,107 @@
+/*
+ * colorloop
+ *
+ * When `options.sceneId` is active, cycle all scene lights through all
+ * colors in scene.
+ *
+ * DEPENDENCIES:
+ *
+ * - scene-spy
+ *
+ * SETUP:
+ *
+ * - Register dependencies and plugin in index.js:
+ *
+ * ```
+ * server.register(
+ *   [
+ *     {
+ *       register: require('./plugins/scene-spy'),
+ *       options: { groups: [ 0, 1 ] }
+ *     },
+ *     {
+ *       register: require('./plugins/scenes/colorloop'),
+ *       options: { sceneId: '<id of a Hue scene>' }
+ *     ...
+ *   ]
+ * ...
+ * ```
+ */
+
+const request = require('request-promise-native');
+const registerScene = require('../scene-spy').registerScene;
+
+const nextColor = state => {
+  const offsetLight = Object.values(state.scene.lightstates)[
+    (state.lightIndex + state.offset) % state.scene.lights.length
+  ];
+
+  const lightId = state.scene.lights[state.lightIndex];
+
+  request({
+    url: `http://${process.env.HUE_IP}/api/${process.env.USERNAME}/lights/${lightId}/state`,
+    method: 'PUT',
+    body: {
+      xy: offsetLight.xy ? offsetLight.xy : undefined,
+      ct: offsetLight.ct ? offsetLight.ct : undefined,
+      transitiontime: Math.round((state.config.delayMs * state.scene.lights.length) / 100),
+    },
+    json: true,
+  });
+
+  state.lightIndex = (state.lightIndex + 1) % state.scene.lights.length;
+
+  if (!state.lightIndex) {
+    // If lightIndex wrapped around, increase offset
+    state.offset = (state.offset + 1) % state.scene.lights.length;
+  }
+
+  state.colorTimeout = setTimeout(() => nextColor(state), state.config.delayMs);
+};
+
+exports.register = async function (server, options, next) {
+  server.dependency(['scene-spy']);
+  const config = options;
+  config.delayMs = config.delayMs || 3000;
+
+  try {
+    const scene = await registerScene(config.sceneId);
+
+    const state = {
+      colorTimeout: null,
+      lightIndex: 0,
+      offset: 0,
+      scene,
+      config,
+    };
+
+    scene.events.on('activate', async () => {
+      state.lightIndex = 0;
+      state.offset = 0;
+
+      clearTimeout(state.colorTimeout);
+      state.colorTimeout = null;
+
+      state.colorTimeout = setTimeout(() => nextColor(state), state.config.delayMs);
+
+      console.log(`colorloop activated for scene ${config.sceneId}`);
+    });
+
+    scene.events.on('deactivate', () => {
+      clearTimeout(state.colorTimeout);
+      state.colorTimeout = null;
+
+      console.log(`colorloop deactivated for scene ${config.sceneId}`);
+    });
+
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.register.attributes = {
+  name: 'scenes/colorloop',
+  version: '1.0.0',
+  multiple: true,
+};
