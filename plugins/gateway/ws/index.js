a78715f7ad1d3f8a72c6c773a22378849dc82a86
Rasmus Eskola <fruitiex@gmail.com>
2017-12-28 21:38:45 +0200

    Add websocket subscription for all luminaires

--- plugins/gateway/ws/index.js
+++ plugins/gateway/ws/index.js
@@ -5,8 +5,15 @@ const register = async function(server, options) {
   await server.register(Nes);
 
   server.subscription('/luminaires/{luminaireId}');
+  server.subscription('/luminaires/all');
+
   server.events.on('luminaireUpdate', luminaire => {
     server.publish(`/luminaires/${luminaire.id}`, luminaire.lights);
+    server.publish(`/luminaires/all`, luminaire);
+  });
+  server.events.on('luminaireDidRegister', luminaire => {
+    server.publish(`/luminaires/${luminaire.id}`, luminaire.lights);
+    server.publish(`/luminaires/all`, luminaire);
   });
 
   server.route({
