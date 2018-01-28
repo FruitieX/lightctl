bb943d92d1c66d9567d00ffb756b69a4fb270236
Rasmus Eskola <fruitiex@gmail.com>
2018-01-28 18:50:17 +0200

    WIP nightlight scene

---
+++ plugins/dynamic-scenes/nightlight.js
@@ -0,0 +1,145 @@
+/*
+ * nightlight
+ */
+
+const forEach = require('lodash/forEach');
+const cloneDeep = require('lodash/cloneDeep');
+const omit = require('lodash/omit');
+const shuffle = require('lodash/shuffle');
+
+const {
+  modifyScene,
+  getSceneLuminaires,
+  getSceneLightCmds,
+} = require('../../src/scenes');
+const { getLuminaires } = require('../../src/lights');
+const state = require('../../src/state');
+
+let scene = null;
+let timeout = null;
+let config = null;
+
+const delay = ms =>
+  new Promise((resolve, reject) => (timeout = setTimeout(resolve, ms)));
+
+const removeTransitiontime = lightstates => {
+  forEach(
+    lightstates,
+    (light, lightId) => (lightstates[lightId] = omit(light, 'transitiontime')),
+  );
+
+  return lightstates;
+};
+
+const fadeBack = async (server, options, prevScene) => {
+  server.events.emit('modifyScene', {
+    sceneId: options.sceneId,
+    payload: {
+      lightstates: removeTransitiontime(prevScene.lightstates),
+    },
+  });
+};
+
+const fadeTime = 30 * 1000;
+
+const runScene = async sceneId => {
+  clearTimeout(timeout);
+
+  const sceneLightCmds = getSceneLightCmds(sceneId, true);
+
+  const restoreLightCmds = [];
+
+  // For each light in the Nightlight scene, find the current light state and
+  // store it in restoreLightCmds
+  sceneLightCmds.forEach(({ light, cmd }) => {
+    restoreLightCmds.push({
+      light,
+      cmd: { hsv: light.getState().currentState },
+    });
+  });
+
+  await delay(1000);
+
+  // Restore lights
+  modifyScene({
+    sceneId,
+    lightCmds: restoreLightCmds,
+  });
+
+  await delay(1000);
+
+  const lightsToFade = shuffle(sceneLightCmds);
+
+  const curSceneCmds = restoreLightCmds;
+
+  // Fade out lights one by one
+  for (const lightCmd of lightsToFade) {
+    /*
+    // For whatever reason, fading a light to 0 brightness and then turning it
+    // off results in a much smoother fade end than simply fading to off state
+    // with a long transitiontime
+    lightstates[lightId] = {
+      ...light,
+      transitiontime: Math.round((fadeTime / lightsToFade.length + 1000) / 100),
+      on: true,
+      bri: light.on ? light.bri : 0,
+      xy: light.xy ? light.xy : [0.6, 0.4],
+    };
+    */
+
+    console.log('Fading out ', lightCmd.light.parentLuminaire.id);
+
+    const lightIndex = curSceneCmds.findIndex(
+      candidate => candidate.light.uuid === lightCmd.light.uuid,
+    );
+
+    curSceneCmds[lightIndex] = lightCmd;
+
+    console.log(curSceneCmds);
+
+    modifyScene({
+      sceneId,
+      lightCmds: curSceneCmds,
+      transitionTime: fadeTime / lightsToFade.length + 1000,
+    });
+
+    await delay(fadeTime / lightsToFade.length);
+  }
+};
+
+const sceneMiddleware = (server, options, scene) => ({
+  sceneId,
+  lightCmds,
+  activated,
+}) => {
+  if (sceneId === options.sceneId && activated) {
+    lightCmds.forEach(lightCmd => {
+      const { cmd } = lightCmd;
+
+      // Instant transition to black to indicate activation
+      cmd.rgb = [0, 0, 0];
+
+      // TODO: some way of handling this color conversion crap
+      delete cmd.xyY;
+      delete cmd.ct;
+      delete cmd.hsv;
+    });
+
+    runScene(options.sceneId);
+  }
+};
+
+const register = async function(server, options) {
+  server.events.on('start', () => {
+    server.events.on(
+      'sceneMiddleware',
+      sceneMiddleware(server, options, scene),
+    );
+  });
+};
+
+module.exports = {
+  name: 'scenes/nightlight',
+  version: '1.0.0',
+  register,
+};
